283090a9e3066dc252939f82d7a139668e484e42
dad <donjwhite1@gmail.com>
2025-07-20 20:42:11 -0600

    first commit

---
+++ src/main/java/com/example/SampleRest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRest {
+
+    public static final Logger LOG = LoggerFactory.getLogger(SampleRest.class);
+
+    @GetMapping(value = "/motd")
+    public String getMessage() {
+        LOG.info("Entering getMessage");
+        return "Hello World";
+    }
+}
